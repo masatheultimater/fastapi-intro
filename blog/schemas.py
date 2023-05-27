640157f6d73e2428704b53d822ba94fec1e0935e
masatheultimater@gmail.com <masatheultimater@gmail.com>
2023-05-27 22:52:08 +0900

    JWT認証実装

--- blog/schemas.py
+++ blog/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class BlogBase(BaseModel):
@@ -34,6 +35,16 @@ class ShowBlog(BaseModel):
     class Config:
         orm_mode = True
 
+
 class Login(BaseModel):
     email: str
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: Optional[str] = None
---
+++ blog/token.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = "76b2baac6244d58ec777f733c1b64ac47bed23a91ef597a31d1ac1d07e0b8da1"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
